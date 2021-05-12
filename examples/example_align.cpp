4e737f2bef146e86ec2606918bd502ed93cc1719
Maksim Gorokhovskiy <magor1744@gmail.com>
2021-05-12 20:34:27 +0300

    cpp client

--- examples/example_align.cpp
+++ examples/example_align.cpp
@@ -1,21 +1,26 @@
+#include "cpp_client.h"
 #include "bits/stdc++.h"
 
 using namespace std;
+using namespace rviewer;
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    cout << "size (30,30)\n";
+    Init().size({30, 30});
     // default align is CC
-    cout << "grid c=(15,15) d=(3,3) s=(30,30) col=(255,255,255)\n";
+    Grid().center({15, 15}).size({30, 30}).dimensions({3, 3}).color(Color::white);
     string s = "BCE";
     vector<double> v = {5, 15, 25};
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            cout << "rect c=(" << v[i] << "," << v[j] << ") s=(4,3) f=0 col=(255,0,0) a=" << s[i] << s[j] << "\n";
-            cout << "grid c=(" << v[i] << "," << v[j] << ") s=(3,2) d=(3,2) f=0 col=(0,255,0) a=" << s[i] << s[j] << "\n";
-            cout << "text c=(" << v[i] << "," << v[j] << ") s=1 col=(0,255,255) a=" << s[i] << s[j] << " m=" << s[i] << s[j] << "\n";
-            cout << "circle c=(" << v[i] << "," << v[j] << ") r=0.2 f=1 col=(255,255,255)\n";
+            string tmp;
+            tmp += s[i];
+            tmp += s[j];
+            Rect({v[i], v[j]}, {4, 3}).fill(0).color(Color::red).align(s[i], s[j]);
+            Grid({v[i], v[j]}, {3, 2}, {3, 2}).color(Color::green).align(s[i], s[j]);
+            Text(tmp).center({v[i], v[j]}).color(Color::cyan).align(s[i], s[j]);
+            Circle({v[i], v[j]}, 0.2).fill(1).color(Color::white);
         }
     }
 
